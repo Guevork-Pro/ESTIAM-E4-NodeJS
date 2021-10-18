a3c4c2c4d2a70d184047e4cb2f1070cbce1a79c5
Guévork <guevork.djaladian@keewe.eu>
2021-10-18 15:33:26 +0200

    Correction exercice 5

--- index.js
+++ index.js
@@ -1,151 +1,71 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
+const vehicles = require('./vehicles.json');
 
-const vehicles = [
-  {
-    id: 1,
-    type: "car",
-    brand: "Lambo",
-    model: "Aventador",
-    color: "Jaune",
-    year: 2020
-  },
-  {
-    id: 2,
-    type: "bike",
-    brand: "Estiam",
-    model: "double",
-    color: "red",
-    year: 2010
+let nextId = 3;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (request, response) => {
+  response.send("Racine");
+});
+
+app.get('/vehicles', (request, response) => {
+  response.send(vehicles);
+});
+
+app.get('/vehicles/:typeVehicle', (request, response) => {
+  const typeVehicle = request.params.typeVehicle;
+  const filterByType = (vehicle) => {
+    return vehicle.type === typeVehicle;
   }
-];
+  const vehiclesFilteredByType = vehicles.filter(filterByType);
+  response.send(vehiclesFilteredByType);
+});
 
-const createVehicle = (type, { brand, model, color, year }) => {  
-  if(!brand || !model || !color || !year) {
-    return undefined;
-  }
+app.get('/vehicles/:typeVehicle/:id', (request, response) => {
+  const typeVehicle = request.params.typeVehicle;
+  const idVehicle = Number(request.params.id);
 
-  return {
-    id: vehicles.length + 1,
-    type,
-    brand,
-    model,
-    color,
-    year
-  }
-}
+  const vehiclesFilteredByTypeAndId = vehicles.filter(
+    (vehicle) => {
+      return vehicle.type === typeVehicle && vehicle.id === idVehicle;
+    }
+  );
+  response.send(vehiclesFilteredByTypeAndId);
+});
 
-// Gère les requêtes GET
-const handleGetRequest = (request, response) => {
-  const url = new URL(request.url, `http://${request.headers.host}`);
-  const { pathname } = url;
-
-  if(pathname === "/") {
-    response.end(
-      "GET: /,\nGET: /vehicles,\nGET: /vehicles/:type,\nGET: /vehicles/:type/:id,\nPOST: /vehicles/:type,\nPUT: /vehicles/:type,\nDELETE: /vehicles/:type/:id"
-    );
-    return;
-  }
-  else if (pathname.startsWith("/vehicles")) {
-    const pathnameParts = pathname.split("/");
-
-    const getAllVehicles = pathnameParts.length === 2;
-    const getVehiclesFromType = pathnameParts.length === 3;
-    const getVehiclesFromTypeAndId = pathnameParts.length === 4;
-
-    response.writeHead(200, { 'Content-Type' : 'application/json' });
-
-    if (getAllVehicles) {
-      response.end(
-        JSON.stringify({ vehicles })
-      );
+app.use('/vehicles/:typeVehicle', (request, response, next) => {
+  if(request.method === "POST") {
+    if(Object.values(request.body).length === 0) {
+      response.sendStatus(400);
       return;
     }
-    else if(getVehiclesFromType) {
-      const vehicleType = pathnameParts[2];
-      const filteredVehicles = vehicles.filter(
-        ({ type }) => type === vehicleType        
-      );
-      response.end(JSON.stringify({ vehicles: filteredVehicles }));
-      return;
-    }
-    else if(getVehiclesFromTypeAndId) {
-      const vehicleType = pathnameParts[2];
-      const vehicleId = Number(pathnameParts[3]);
-      const filteredVehicles = vehicles.filter(
-        ({ id, type }) => type === vehicleType && id === vehicleId
-      );
-      response.end(JSON.stringify(filteredVehicles[0]));
+    else if (!request.body.brand || !request.body.model || !request.body.color || !request.body.year) {
+      response.sendStatus(400);
       return;
     }
   }
+  next();
+})
 
-  response.writeHead(404);
-  response.end("Not found");
-}
+app.post('/vehicles/:typeVehicle', (request, response) => {
+  const typeVehicle = request.params.typeVehicle;
 
-// Gère les requêtes POST
-const handlePostRequest = (request, response) => {
-  const url = new URL(request.url, `http://${request.headers.host}`);
-  const { pathname } = url;
+  const newVehicle = {
+    id: nextId,
+    type: typeVehicle,
+    ...request.body
+  };
 
-  const pathnameParts = pathname.split("/");
- 
-  if(pathname.startsWith("/vehicles") && pathnameParts.length === 3) {
-    let data = "";
-    request.on('data', (chunk) => {
-      data += chunk.toString();
-    });
+  nextId++;
 
-    request.on('end', () => {
-      const vehicleType = pathnameParts[2];
-      const newVehicle = createVehicle(vehicleType, JSON.parse(data));
-      if(newVehicle) {
-        vehicles.push(newVehicle);
+  vehicles.push(newVehicle);
+  response.send(newVehicle);
+})
 
-        response.writeHead(201, { 'Content-Type' : 'application/json' }); 
-        response.end(JSON.stringify(newVehicle));
-      } else {
-        response.writeHead(400);
-        response.end("Bad request");
-      }
-    });
-    return;
-  }
-  response.end("J'ai reçu un POST")
-}
-
-// Gestionnaire des requêtes 
-// (appelle la fonction adéquate pour le type de requête reçu)
-function handleRequest(request, response) {
-  const requestType = request.method; 
-
-  switch (requestType) {
-    case 'GET':
-      handleGetRequest(request, response);
-      break;
-
-    case 'POST':
-      handlePostRequest(request, response);
-      break;
-
-    case 'PUT':
-      response.end("J'ai reçu un PUT")
-      break;
-
-    case 'DELETE':
-      response.end("J'ai reçu un DELETE")
-      break;
-
-    default:
-      response.writeHead(404);
-      response.end("Je ne gère pas ce type de requête");
-      break;
-  }
-}
-
-// Crée le serveur HTTP
-const httpServer = http.createServer(handleRequest);
-
-httpServer.listen(3030, () => {
-  console.log("Server listening on port 3030");
+app.listen(port, () => {
+  console.log(`Server started on port 3000`);
 })
